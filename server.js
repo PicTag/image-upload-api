7e91937a3d06ec3b0a1639646e7993617fb89436
YuanSamuel <samuel6161@gmail.com>
2022-01-16 22:38:18 -0600

    :bug: add 404 and update port

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 import router from "./api/routes/storageRoutes.js";
 
 app.use("/api", router);
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
