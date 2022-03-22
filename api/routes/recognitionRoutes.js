73d33ced245609834f68abc9a988b9c69b47fd79
YuanSamuel <samuel6161@gmail.com>
2022-03-21 21:35:44 -0500

    add logs

--- api/routes/recognitionRoutes.js
+++ api/routes/recognitionRoutes.js
@@ -7,11 +7,15 @@ const upload = multer();
 
 router.get('/classify', upload.array('files', 12), async function (req, res) {
   try {
+    console.log('got req');
     const files = req.files;
     let puts = [];
+    console.log('here');
+    console.log(files);
     files.forEach((file) => puts.push(classifyImage(file)));
 
     const values = await Promise.all(puts);
+    console.log(values);
     res.json(values);
   } catch (error) {
     res.send(error.message);
